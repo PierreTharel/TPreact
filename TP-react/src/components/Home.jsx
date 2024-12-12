354c6ba06d6bb6b81ef47a1b9d536551b50bec83
Pierre-Olivier Tharel <pierre.tharel@gmail.com>
2024-12-12 11:54:42 +0100

    Fonctionnalité du bouton Voir le jeu en detail

--- TP-react/src/components/Home.jsx
+++ TP-react/src/components/Home.jsx
@@ -5,14 +5,16 @@ function Home() {
   return (
     <>
 
-    <h1>Bienvenue sur mon site d'actualité Nintendo</h1>
+    <h1>Bienvenue sur mon "Fan-Site" Nintendo, l'univers Nintendo à porter de main !</h1>
 
     <Card>
         <h2>Super Mario Jamboree</h2>
         <h4>Nintendo Switch</h4>
         <h3>Jouez en famille ou entre amis et defiez vous dans des mini-jeux 
             diverstissant sur Nintendo Switch !</h3>
-        <button>Voir le jeu en detail</button>
+        <button onClick={() => {
+        alert('Super Mario Jamboree, Party-Game, 17/10/2024, 44,99€');
+        }}>Voir le jeu en detail</button>
     </Card>
 
     <Card>
@@ -21,7 +23,9 @@ function Home() {
         <h3>Faites appel à la sagesse de la princesse Zelda pour sauver le royaume d'Hyrule dans The Legend of Zelda: Echoes of Wisdom sur Nintendo Switch. 
             Utilisez ses nouveaux pouvoirs de façon créative pour explorer le vaste royaume, affronter des monstres, traverser 
             des donjons et enquêter sur les mystérieuses failles qui se sont ouvertes.</h3>
-        <button>Voir le jeu en détail</button>
+            <button onClick={() => {
+        alert('The Legend of Zelda: Echoes of Wisdom, RPG, 26/09/2024, 59,99€');
+        }}>Voir le jeu en detail</button>
     </Card>
 
     <Card>
@@ -32,7 +36,9 @@ function Home() {
             prenez part à des combats au tour par tour dans Mario & Luigi : 
             L'épopée fraternelle sur Nintendo Switch.
         </h3>
-        <button>Voir le jeu en détail</button>
+        <button onClick={() => {
+        alert('Mario et Luigi : l epopee fraternelle, RPG, 07/11/2024, 49,99€');
+        }}>Voir le jeu en detail</button>
     </Card>
 
     </>
